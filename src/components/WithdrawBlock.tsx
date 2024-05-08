66ffc97055e9ffc3a5b62dcd795b2c2310e8a93f
walletflutter <116666000+dmitri-ross@users.noreply.github.com>
2024-05-08 15:50:40 +0300

    fix some

--- src/components/WithdrawBlock.tsx
+++ src/components/WithdrawBlock.tsx
@@ -1,11 +1,16 @@
-import { Button, Input, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from "@nextui-org/react";
+import {
+  Button,
+  Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  useDisclosure,
+} from "@nextui-org/react";
 import { useEffect, useState } from "react";
 
-import {
-  Web3Button,
-  useAddress,
-  useContract
-} from "@thirdweb-dev/react";
+import { Web3Button, useAddress, useContract } from "@thirdweb-dev/react";
 import { BigNumber, ethers } from "ethers";
 export const WithdrawBlock = (symbol) => {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
@@ -68,39 +73,60 @@ export const WithdrawBlock = (symbol) => {
   return (
     <>
       {symbol.symbol == "RUB" && (
-        <div className="w-full flex flex-col gap-4">
-          <div className=" dark flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
-            <Input
-              className="mg-top-20 "
-              type="bankName"
-              label="Наименование банка:"
-              placeholder="Введите наименование банк"
-              onValueChange={setBankName}
-            />
-            <Input
-              type="bik"
-              label="БИК:"
-              placeholder="Введите БИК"
-              required
-              onValueChange={setBik}
-            />
-            <Input
-              type="accountNumber"
-              label="Расчетный счет (р/с):"
-              placeholder="Введите номер счета"
-              onValueChange={setAccountNumber}
-              required
-            />
-            <Input
-              type="number"
-              label="Сумма вывода:"
-              min="1"
-              required
-              placeholder="Укажите сумму"
-              onValueChange={setWithdrawAmount}
-            />
+        <>
+          <div className="w-full flex flex-col gap-4">
+            <div className=" dark flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
+              <Input
+                className="mg-top-20 "
+                type="bankName"
+                label="Наименование банка:"
+                placeholder="Введите наименование банк"
+                onValueChange={setBankName}
+              />
+              <Input
+                type="bik"
+                label="БИК:"
+                placeholder="Введите БИК"
+                required
+                onValueChange={setBik}
+              />
+              <Input
+                type="accountNumber"
+                label="Расчетный счет (р/с):"
+                placeholder="Введите номер счета"
+                onValueChange={setAccountNumber}
+                required
+              />
+              <Input
+                type="number"
+                label="Сумма вывода:"
+                min="1"
+                required
+                placeholder="Укажите сумму"
+                onValueChange={setWithdrawAmount}
+              />
+            </div>
           </div>
-        </div>
+          <Web3Button
+            className="checkDis"
+            contractAddress={
+              allowance && allowance?.gte(BigNumber.from(withdrawAmount))
+                ? withdrawContractAddress
+                : tokenAddress
+            }
+            action={handleWithdraw}
+            onSuccess={async () => {
+              console.log("Approved");
+              await checkApprove();
+            }}
+          >
+            {BigNumber.from(withdrawAmount).gt(0)
+              ? allowance && allowance?.gte(BigNumber.from(withdrawAmount))
+                ? "Подтвердить вывод (2/2)"
+                : "Разрешить расходование (1/2)"
+              : "Введите сумму"}
+          </Web3Button>
+        </>
       )}
 
       {symbol.symbol != "RUB" && (
@@ -114,23 +140,6 @@ export const WithdrawBlock = (symbol) => {
         </div>
       )}
 
-      <Web3Button
-        className="checkDis"
-        contractAddress={
-          allowance && allowance?.gte(BigNumber.from(withdrawAmount))
-            ? withdrawContractAddress
-            : tokenAddress
-        }
-        action={handleWithdraw}
-        onSuccess={async () => {
-          console.log("Approved");
-          await checkApprove();
-        }}
-      >
-        {allowance && allowance?.gte(BigNumber.from(withdrawAmount))
-          ? "Подтвердить вывод (2/2)"
-          : "Разрешить расходование (1/2)"}
-      </Web3Button>
       <Modal className="dark" isOpen={isOpen} onOpenChange={onOpenChange}>
         <ModalContent>
           {(onClose) => (

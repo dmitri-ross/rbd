e3b1bba8497c19e22387e4886fdc1bc2df4d6fec
walletflutter <116666000+dmitri-ross@users.noreply.github.com>
2024-05-16 15:17:47 +0300

    add lang

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,10 +1,10 @@
 import styles from "../styles/Home.module.css";
 import { Button, ButtonGroup } from "@nextui-org/button";
 import { useRouter } from "next/router";
-
+import { useTranslation } from 'react-i18next';
 export const TopMenu = ({ symbol = "" }) => {
   const router = useRouter();
-
+  const { t } = useTranslation();
   const handleNavigation = (url) => {
     router.push(url);
   };
@@ -35,7 +35,7 @@ export const TopMenu = ({ symbol = "" }) => {
               clip-rule="evenodd"
             ></path>
           </svg>
-          Перевести
+          {t('send')}
         </Button>
 
         <Button
@@ -61,7 +61,7 @@ export const TopMenu = ({ symbol = "" }) => {
               d="M7.5 21 3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5"
             />
           </svg>
-          Обмен
+          {t('swap')}
         </Button>
 
         <Button
@@ -86,7 +86,7 @@ export const TopMenu = ({ symbol = "" }) => {
               d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3"
             />
           </svg>
-          Пополнить
+          {t('deposit')}
         </Button>
 
         <Button
@@ -113,7 +113,7 @@ export const TopMenu = ({ symbol = "" }) => {
               d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z"
             />
           </svg>
-          Вывести
+          {t('withdraw')}
         </Button>
       </div>
       {/* <ButtonGroup

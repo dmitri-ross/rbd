1d5a21726e705c83d102eb88df829088b4ac70ff
dmitri-ross <116666000+dmitri-ross@users.noreply.github.com>
2024-05-21 19:59:33 +0300

    fix wd contract

--- const/contracts.ts
+++ const/contracts.ts
@@ -1,7 +1,7 @@
 // Replace this with your contract address
 export const contractAddresses = { "RUB": "0xe546AE1D6c9b8F5B7AE856Da9E9148Db05564B94", "USD": "0x54bccB971FeB8917d80a252D20A1725a6DbE0BE4", "INR": "0x30137348b9e8bFFbC476Dcf06A5383150cD3443E" }
 export const explorerBaseURL = "https://sepolia.etherscan.io/tx/"
-export const withdrawContractAddress = "0x0De54BBd383811dC1461D2943e3C5Ab2751414cB";
+export const withdrawContractAddress = "0x91700514F6b7db499Ce1cDb745208a05CB0B2C5A";
 // Replace this with your domain name.
 export const domainName = "localhost";
 

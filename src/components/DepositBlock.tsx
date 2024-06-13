2a75387208b9728d3e893a04863e6cbdd31e401c
dmitri-ross <149528176+restifi@users.noreply.github.com>
2024-06-13 20:07:21 +0300

    fix rub deposit

--- src/components/DepositBlock.tsx
+++ src/components/DepositBlock.tsx
@@ -28,10 +28,21 @@ export const DepositBlock = (symbol) => {
             <p>{t('bank')}: <i> ПАО Сбербанк</i></p>
             <p>{t('account')}: <i>40702810038000470531</i></p>
             <p>{t('bic')}: <i>044525225</i></p>
-            <p>{t('paymentPurpose')}: <i>{user?.address}</i></p>
+            <p>{t('paymentPurpose')}: <i>Пополнение {user?.address}</i></p>
           </>}
 
-          {symbol.symbol === 'INR' && <>
+          {symbol.symbol  != "RUB" && (
+        <div className="w-full flex flex-col gap-4">
+          <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
+            <p>
+              <h3 className="danger">Временно недоступно! </h3>
+              <h4 className="danger">Выберите другую валюту.</h4>
+            </p>
+          </div>
+        </div>
+      )}
+
+          {/* {symbol.symbol === 'INR' && <>
             <p>{t('bankDetailsINR')}</p>
             <p>{t('bank')}: State Bank of India</p>
             <p>{t('account')}: 31107460283</p>
@@ -45,7 +56,7 @@ export const DepositBlock = (symbol) => {
             <p>{t('account')}: 4426324623</p>
             <p>{t('routingNumber')}: 026009593</p>
             <p>{t('paymentPurpose')}: <i>{user?.address}</i></p>
-          </>}
+          </>} */}
         </CardBody>
         <Divider />
 
